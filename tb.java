4281dbce41484665a103f3f5116f224d4879fbcb
Owen Hamnett <owham09@gmail.com>
2024-09-19 15:49:14 -0400

    Fixed formatting. Also figured out how to sync with git on home computer

--- tb.java
+++ tb.java
@@ -9,57 +9,62 @@ import java.util.Scanner;
 
 public class tb
 {
-static Scanner scammy = new Scanner(System.in);
-
-
-
-public static int getInt(String message)//this method gets an int and adds a message
-{
-print(message);
-int myInt = scammy.nextInt();
-return myInt;
-}
-
-public static double getDouble(String message)//this method gets a double and adds a message
-{
-print(message);
-double myDouble = scammy.nextDouble();
-return myDouble;
-}
-
-public static char getChar(String message)//this method gets a char and adds a message
-{
-scammy.nextLine();
-print(message);
-char myChar = scammy.nextLine().charAt(0);
-return myChar;
-}
-
-public static String getString(String message)//this method gets a string and adds a message
-{
-scammy.nextLine();
-print(message);
-String myString = scammy.nextLine();
-return myString;
-}
-
-public static double round(double num, double place)//this is a rounding class
-{
-place = Math.pow(10.0, place);
-double rounded = (int)(num * place + 0.5) / place;
-return rounded;
-}
-  public static void print(String string)//this method prints
-{
-System.out.println(string);
-}
-  public static void print(int a){
-    System.out.println(a);
+    static Scanner scammy = new Scanner(System.in);
+    
+    public static int getInt(String message)//this method gets an int and adds a message
+    {
+        print(message);
+        int myInt = scammy.nextInt();
+        return myInt;
     }
-        public static void print(double a){
-    System.out.println(a);
+    
+    public static double getDouble(String message)//this method gets a double and adds a message
+    {
+        print(message);
+        double myDouble = scammy.nextDouble();
+        return myDouble;
     }
-        public static void print(char a){
-    System.out.println(a);
+    
+    public static char getChar(String message)//this method gets a char and adds a message
+    {
+        scammy.nextLine();
+        print(message);
+        char myChar = scammy.nextLine().charAt(0);
+        return myChar;
+    }
+    
+    public static String getString(String message)//this method gets a string and adds a message
+    {
+        scammy.nextLine();
+        print(message);
+        String myString = scammy.nextLine();
+        return myString;
+    }
+    
+    public static double round(double num, double place)//this is a rounding class
+    {
+        place = Math.pow(10.0, place);
+        double rounded = (int)(num * place + 0.5) / place;
+        return rounded;
+    }
+    
+    public static void print(String string)//this method prints
+    {
+        System.out.println(string);
+    }
+    
+    public static void print(int a)
+    {
+        System.out.println(a);
+    }
+    
+    public static void print(double a)
+    {
+        System.out.println(a);
+    }
+        
+    public static void print(char a)
+    {
+        System.out.println(a);
     }
 }
