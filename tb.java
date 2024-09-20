33317a6f6c84e8b5ec6a04b23816a9559a33b3de
Owen Hamnett <owham09@gmail.com>
2024-09-19 23:33:28 -0400

    newPrint() allows the use of any amount of any kind of variable

--- tb.java
+++ tb.java
@@ -70,5 +70,31 @@ public class tb
     {
         System.out.println(a);
     }
+    
+    //this is a new and improved print method, will replace old one eventually
+    public static void newPrint(String a, Object... args)//sooo turns out there is a way to add infinite inputs of any type
+    {       
+        String result = String.format(a,args);
+        System.out.println(result);
+        
+        //made an entire working method by myself but turns out there is a built in feature, you just have to use %f in your string
+        //doing %f.1 will do it to one decimal place, %s for string and so on
+        
+        // int[] pos = new int[6];
+
+        // int length = 0;
+        
+        // for(int i = 0; i < 6; i+=2)//this is an optional use of loops and arrays to expand compactly
+        // {
+            // pos[i] = a.indexOf("{", length);    
+            // pos[i + 1] = a.indexOf("}", length);
+            
+            // length = pos[i + 1] + 1;
+        // }
+        
+        // String newString = a.substring(0,pos[0]) + b + a.substring(pos[1] + 1, pos[2]) + c + a.substring(pos[3] + 1, pos[4]) + d + a.substring(pos[5] + 1);
+        
+        // System.out.println(newString);
+    }
 }
 
