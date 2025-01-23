804edf8e97f07b9f863de793d719776bef29f00b
Oham09 <160649743+Oham09@users.noreply.github.com>
2025-01-23 14:06:11 -0500

    Update tb.java

--- tb.java
+++ tb.java
@@ -107,7 +107,7 @@ public class tb
     public static int random(int sp, int ep)
     {
         double rnd = Math.random();
-        double dif = Math.abs(ep - sp);
+        double dif = Math.abs(ep - sp) + 1;
         int random = (int)(rnd * dif) + sp;
         return random;
     }   
@@ -127,6 +127,103 @@ public class tb
         }
         if (num <= 0) prime = false;
         return prime;
+    }
+
+    public static int[] setArray(int size)
+    {
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++)
+        {
+            int num = 101;
+
+            while (num > 100)//this is an easy way to error check it
+            {
+                System.out.println("What number goes in array position " + (i + 1) + "?");
+                num = scammy.nextInt();
+            }
+
+            array[i] = num;
+        }
+
+        return array;
+    }
+
+    public static int getMode(int[] array)//this uses the frequencyArray method
+    {
+        int[] frequencyArray = frequencyArray(array);
+        
+        int maxValue = Integer.MIN_VALUE;
+        int maxTemp = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < frequencyArray.length; i++)
+        {
+            if (frequencyArray[i] > maxTemp)
+            {
+                maxTemp = frequencyArray[i];
+                maxValue = i;
+            }
+        }
+        
+        if (maxTemp < 2) maxValue = 0;//this means there is no mode
+        
+        return maxValue;
+    }
+
+    public static int[] frequencyArray(int[] array)//i missed this in class so I just redid what you did in the lesson on canvas
+    {
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] > max) max = array[i];
+        }
+
+        int[] temp = new int[max + 1];//add one so that it gets to the correct value
+
+        for (int i = 0; i < array.length; i++)
+        {
+            temp[array[i]]++;
+        }
+        
+        return temp;
+    }
+    
+    public static int[] setRandomArray(int size, int sp, int ep)
+    {
+        int[] array = new int[size];
+        
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = (int)(Math.random() * (ep - sp) + 1) + sp;
+        }
+        
+        return array;
+    }
+    
+    public static void printArray(int[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.println(array[i] + " ");//just uses a space here
+        }
+    }
+    
+    public static int[] arraySort(int[] array)
+    {
+        for (int i = 1; i < array.length; i++)
+        {
+            for (int k = 0; k < array.length - i; k++)
+            {
+                if (array[k] > array[k + 1])
+                {
+                    int temp = array[k];
+                    array[k] = array[k + 1];
+                    array[k + 1] = temp;
+                }
+            }
+        }
+        return array;
     }
 }
 
